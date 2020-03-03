63b64740cc1e77161791eb09bfc2779d999ba2cc
iamakawa <theiooori@gmail.com>
2020-03-03 17:04:03 +0900

    ver 0.9.1 add searchbox

--- map.js
+++ map.js
@@ -1,21 +1,44 @@
 var NumAllEvtCount;
 var NumTodayEvtCount;
+var CancelledEvtList =
+    "https://script.google.com/macros/s/AKfycbz51mpjVZ-XfHTti5Q-fFwzHaRaY_P1ZajawHXxnXnZsynYBq17/exec";
 
 var map = L.map("map").fitWorld();
-map.setView([35.71, 139.75], 14);
-map.on("mousemove", onMapMousemove);
+var geoJsonLayer;
+var geoJsonLayerGroup =  L.layerGroup([]);
 
-L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1
-    }
-).addTo(map);
+window.onload = function(){
+    map.setView([35.71, 139.75], 14);
+    map.on("mousemove", onMapMousemove);
+    
+    L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1
+        }
+    ).addTo(map);
+    fetch(CancelledEvtList)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        countReset()
+        geoJsonLayer = L.geoJSON(myJson, {
+            style: function(feature) {
+                return feature.properties && feature.properties.style;
+            },
+            onEachFeature: onEachFeature
+        }).addTo(map);
+        geoJsonLayerGroup.addTo(map);
+        geoJsonLayerGroup.addTo(geoJsonLayer)
+    });
+   
+}
 
 var latloninfo = L.control({ position: "topright" });
 latloninfo.onAdd = function(map) {
@@ -32,6 +55,39 @@ latloninfo.onAdd = function(map) {
     return this.ele;
 };
 latloninfo.addTo(map);
+
+var searchbox = L.control({ position: "topright" });
+searchbox.onAdd = function(map) {
+    //divを作成
+    this.ele = L.DomUtil.create("input", "searchbox");
+    //後で使うためにidを設定
+    this.ele.id = "searchbox";
+    this.ele.onkeypress = function(e) {
+        fetch(CancelledEvtList)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            geoJsonLayer.clearLayers();
+		    geoJsonLayerGroup.removeLayer(geoJsonLayer);
+            countReset()
+            var searchbox = document.getElementById("searchbox");
+            L.geoJSON(myJson, {
+                style: function(feature) {
+                return feature.properties && feature.properties.style;
+                },
+            filter: function(feature, layer) {   
+                return (feature.properties.name.match(searchbox.value));
+            },
+            onEachFeature: onEachFeature
+        }).addTo(map);
+        geoJsonLayerGroup.addTo(map);
+        geoJsonLayerGroup.addTo(geoJsonLayer)
+    });
+    }
+    return this.ele;
+};
+searchbox.addTo(map);
 
 function onEachFeature(feature, layer) {
     var popupContent =
@@ -82,24 +138,6 @@ function onMapMousemove(e) {
     box.innerHTML = html;
     box.style.visibility = "visible";
 }
-var CancelledEvtList =
-    "https://script.google.com/macros/s/AKfycbz51mpjVZ-XfHTti5Q-fFwzHaRaY_P1ZajawHXxnXnZsynYBq17/exec";
-
-fetch(CancelledEvtList)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        NumAllEvtCount = 0;
-        NumTodayEvtCount = 0;
-        L.geoJSON(myJson, {
-            style: function(feature) {
-                return feature.properties && feature.properties.style;
-            },
-
-            onEachFeature: onEachFeature
-        }).addTo(map);
-    });
 
 function getNowYMD() {
     var dt = new Date();
@@ -108,4 +146,10 @@ function getNowYMD() {
     var d = ("00" + dt.getDate()).slice(-2);
     var result = y + "/" + m + "/" + d;
     return result;
+}
+
+function countReset()
+{
+    NumAllEvtCount = 0;
+    NumTodayEvtCount = 0;
 }
